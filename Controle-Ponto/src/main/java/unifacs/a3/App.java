2d4178033a152794cd24b2696b389205362c8fa7
Daniela Silva <168598759+danicretense@users.noreply.github.com>
2025-09-02 22:46:39 +0000

    Metodo que registra hora de entrada criado com sucesso

--- Controle-Ponto/src/main/java/unifacs/a3/App.java
+++ Controle-Ponto/src/main/java/unifacs/a3/App.java
@@ -21,57 +21,39 @@ public class App
         Scanner scan=new Scanner(System.in);
         System.out.println( "Selecione uma opção:\n1-Registrar ponto\n2-Cadastrar Funcionario\n3-Recuperar acesso" );
         int opcao=scan.nextInt();
-        boolean user_cadastrado=false;
+     
+        //--------------------------------------------------------------
         switch (opcao) {
             case 1:
-            int id=0;
              System.out.println("Digite seu  nome:");
              scan.nextLine();
               String nome_user= scan.nextLine();
-                System.out.println("Digite sua senha senha:");
-                int senha_user=scan.nextInt();
-                Connection connect=conexao_BD();
-                Statement stmt=connect.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT * FROM usuario");
-                while (rs.next()) {
-                if(rs.getString("nome").equals(nome_user) && senha_user==rs.getInt("senha") ){
-                 user_cadastrado=true;
-                  id=rs.getInt("id");
-
-                }else{
-                     System.out.println("Usuario não cadastrado")  ;           
-            }
-        }        
-            if(user_cadastrado)
+              System.out.println("Digite sua senha senha:");
+              int senha_user=scan.nextInt();
+              Resultado r1= verificaUser(nome_user, senha_user) ;      
+            if(r1.user_encontrado())
             System.out.println("1-Entrada\n2-Saída");
             int resposta=scan.nextInt();
              if(resposta==1){
                 LocalDateTime entrada= LocalDateTime.now();
-                  PreparedStatement ps = connect.prepareStatement("INSERT INTO horarios (usuario_id, entrada) VALUES (?, ?)"); 
-            ps.setInt(1, id);
-           ps.setObject(2, entrada);
-            ps.executeUpdate();
-
+              boolean registrado= registra_Hora(entrada,r1.id_user());
+               if(registrado){
+                System.out.println("Sua entrada foi registada com sucesso");
+               }  
              }else{
-                
-                 rs = stmt.executeQuery("SELECT id FROM horarios WHERE saida IS NULL ORDER BY id DESC LIMIT 1");
+                Connection connect= conexao_BD();
+                Statement stmt= connect.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT id FROM horarios WHERE saida IS NULL ORDER BY id DESC LIMIT 1");
                  if (rs.next()){
                     int id_buscado=rs.getInt("id");
                   PreparedStatement   ps = connect.prepareStatement("UPDATE horarios SET saida = ? WHERE id = ?");
                    ps.setObject(1,Timestamp.valueOf(LocalDateTime.now()));
                    ps.setInt(2, id_buscado);
                     ps.executeUpdate();
-
-
-                  
-                    
-                 }
-
-             }
-
-            
-        
-                break;
+                    System.out.println("Saida registrada com sucesso");
+                }
+                    }
+                   break;
             case 2:
              System.out.println("Digite o nome do funcionário:");
               scan.nextLine();
@@ -107,7 +89,6 @@ String user = "postgres.iczuamsiqqdvifexshbf";
 String password = "miniProject25@";
    Connection conn=null;
        try {
-            // O Driver é carregado automaticamente pelo Maven, não precisa Class.forName()
              conn = DriverManager.getConnection(url, user, password);
             System.out.println("Conexão bem-sucedida com o PostgreSQL!");
             
@@ -119,22 +100,34 @@ String password = "miniProject25@";
     }
 
 
-    public static boolean verificaUser(String nome_user, int senha_user) throws Exception{
+    public static Resultado verificaUser(String nome_user, int senha_user) throws Exception{
           Connection connect=conexao_BD();
            boolean user_cadastrado=false;
-
+                  int id=0;
                 Statement stmt=connect.createStatement();
                 ResultSet rs = stmt.executeQuery("SELECT * FROM usuario");
                 while (rs.next()) {
                 if(rs.getString("nome").equals(nome_user) && senha_user==rs.getInt("senha") ){
                  user_cadastrado=true;
-                  int id=rs.getInt("id");
+                   id=rs.getInt("id");
 
                 }else{
-                     System.out.println("Usuario não cadastrado")  ;           
+                     System.out.println("Usuario não cadastrado")  ; 
+                     break;          
             }
         }
-         return user_cadastrado;
+         return new Resultado(id,user_cadastrado);
+    }
+
+    public static boolean registra_Hora(LocalDateTime entrada, int usuario_id) throws Exception{
+        Connection connect=conexao_BD();
+        boolean horario_registrado=false;
+        PreparedStatement ps = connect.prepareStatement("INSERT INTO horarios (usuario_id, entrada) VALUES (?, ?)"); 
+            ps.setInt(1, usuario_id);
+           ps.setObject(2, entrada);
+            ps.executeUpdate();
+           return horario_registrado=true;
+
     }
 }
 
