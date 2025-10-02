3e8aa4c4f0f7eaf3ff33a4c7957c0ad7cda9ea8d
amandacafe <amandacaffe@gmail.com>
2025-10-02 20:33:19 -0300

    refactor conexão com BD

--- Controle-Ponto/src/main/java/unifacs/a3/Conexao.java
+++ Controle-Ponto/src/main/java/unifacs/a3/Conexao.java
@@ -17,6 +17,40 @@ public class Conexao {
             return null; // ou lance a exceção para tratar fora
         }
     }
+//cadastro de usuário
+    public static void cadastra_Usuario(String nome, int senha ) throws Exception {
+          try (Connection conectando = Conexao.conexao_BD()) {
+                    PreparedStatement ps = conectando.prepareStatement(
+                        "INSERT INTO usuario (nome, senha) VALUES (?, ?)"
+                    );
+                    ps.setString(1, nome);
+                    ps.setInt(2, senha);
+                    ps.executeUpdate();
+                }
+    }
+
+  //verificação de usuário
+    public static Resultado verificaUser(String nome_user, int senha_user) throws Exception {
+        Connection connect =Conexao.conexao_BD();
+        boolean user_cadastrado = false;
+        int id = 0;
+
+        String sql = "SELECT id FROM usuario WHERE nome = ? AND senha = ?";
+        PreparedStatement ps = connect.prepareStatement(sql);
+        ps.setString(1, nome_user);
+        ps.setInt(2, senha_user);
+
+        ResultSet rs = ps.executeQuery();
+
+        // Se encontrou uma linha, usuário existe
+        if (rs.next()) {
+            user_cadastrado = true;
+            id = rs.getInt("id");
+        
+        }
+
+        return new Resultado(id, user_cadastrado);
+    }
 
 
 }
