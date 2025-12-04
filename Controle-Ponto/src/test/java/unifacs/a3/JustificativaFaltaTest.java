80cbd4c95ae18b81295af208e17a84f2a2087d5a
Daniela Silva <168598759+danicretense@users.noreply.github.com>
2025-12-04 21:08:55 +0000

    test: Corrigido teste do metodo registra_faltaBD()

--- Controle-Ponto/src/test/java/unifacs/a3/JustificativaFaltaTest.java
+++ Controle-Ponto/src/test/java/unifacs/a3/JustificativaFaltaTest.java
@@ -8,7 +8,7 @@ import java.sql.PreparedStatement;
 
 import org.junit.jupiter.api.Test;
 
-public class FaltaBDTest {
+public class JustificativaFaltaTest {
 
     @Test
     void testRegistraFaltaSimples() throws Exception {
