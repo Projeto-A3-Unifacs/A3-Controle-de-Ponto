5b7b36bfdf1ce9058b4b1957d1954e62c91c42e9
Daniela Silva <168598759+danicretense@users.noreply.github.com>
2025-12-05 00:19:45 -0300

    chore: Update commitlint rules for header length

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,5 +2,6 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+    'header-max-length': [2, 'always', 300] 
   },
 };
