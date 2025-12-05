95ca2c8e2ff2d999f5867400f8920839ba86081e
Daniela Silva <168598759+danicretense@users.noreply.github.com>
2025-12-05 00:24:57 -0300

    chore:  Alterado tamanho das mensagens nos commits

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,5 +2,7 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+    'header-max-length': [2, 'always', 300],
+    'body-max-line-length': [2, 'always', 200], // <-- adicionada
   },
 };
